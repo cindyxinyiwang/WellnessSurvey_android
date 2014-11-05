b2689ff22056c7dc480987c631967dc0351e17d0
cindyxinyiwang <xwang29@nd.edu>
2014-11-04 21:52:24 -0500

    submit/cancel
    Add action sheet to answer views to allow users to select OK/cancel
    before submitting.
    Add&synched a text box with slider. Further polish needed.

--- app/src/main/java/com/example/cindywang/wellnesssurvey/Login.java
+++ app/src/main/java/com/example/cindywang/wellnesssurvey/Login.java
@@ -1,19 +1,31 @@
 package com.example.cindywang.wellnesssurvey;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+import java.text.ParseException;
 
 
 public class Login extends Activity {
+
+    FragmentManager fm = getFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Parse.initialize(this, "7y3hW9Gibahq2gCpaTd5TfTH3xlrSao2PCleXr9E", "qVUTlfmiQatz6EvU1gqUBsDqzgO3ajcAGhbXpj4Q");
     }
 
 
@@ -34,6 +46,28 @@ public class Login extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void logIn(){
+        EditText usernameView = (EditText) findViewById(R.id.username);
+        EditText passwordView = (EditText) findViewById(R.id.password);
+        String username = usernameView.getText().toString();
+        String password = passwordView.getText().toString();
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, com.parse.ParseException e) {
+                if (user != null) {
+                    // Hooray! The user is logged in.
+                    Intent i = new Intent(Login.this, QList.class);
+
+                } else {
+                    // Signup failed. Look at the ParseException to see what happened.
+                    DFragment dFragment = new DFragment();
+                    dFragment.show(fm, "Login Error");
+                }
+            }
+        });
+
     }
 
     public void signUp (View view){
